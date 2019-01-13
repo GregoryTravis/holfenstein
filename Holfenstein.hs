40d2f9c08493eb749edb55d77a1db99b2858bae2
Gregory Travis <greg.m.travis@gmail.com>
2019-01-13 11:51:21 -0500

    draw map

--- Holfenstein.hs
+++ Holfenstein.hs
@@ -56,6 +56,7 @@ setAsRenderTarget r Nothing = SDL.rendererRenderTarget r $= Nothing
 setAsRenderTarget r (Just (Texture t _)) = SDL.rendererRenderTarget r $= Just t
 
 data Color = Color Int Int Int
+white = Color 255 255 255
 
 packColor :: Color -> Word32
 packColor (Color r g b) =
@@ -75,8 +76,8 @@ withFramebuffer (Texture t _) f = do
   unlockTexture t
   return result
 
-drawLine :: V2 Int -> V2 Int -> Color -> Ptr Word32 -> Int -> IO ()
-drawLine a@(V2 x0 y0) (V2 x1 y1) color ptr pitch = step fa delta count
+drawLine :: Line -> Color -> Ptr Word32 -> Int -> IO ()
+drawLine (Line a@(V2 x0 y0) (V2 x1 y1)) color ptr pitch = step fa delta count
   where delta | isVert = V2 ((signum dx) * (abs (dx / dy))) (signum dy)
               | otherwise = V2 (signum dx) ((signum dy) * (abs (dy / dx)))
         count | isVert = abs idy
@@ -101,13 +102,18 @@ drawLine a@(V2 x0 y0) (V2 x1 y1) color ptr pitch = step fa delta count
         --bleh :: V2 Int -> V2 Int -> (V2 Double, Int)
         --bleh a b | isVert a b -> ((V2 (dx / dy) 1)
 
+drawLines :: [Line] -> Ptr Word32 -> Int -> IO ()
+drawLines lines ptr pitch =
+  let dl line = drawLine line white ptr pitch
+   in mapM_ dl lines
+
 toOffset (V2 x y) pitch = y * (pitch `div` 4) + x
 
-checkBounds (V2 x y) = x >= 0 && y >= 0 && x < screenWidth && y < screenHeight
+inScreenBounds (V2 x y) = x >= 0 && y >= 0 && x < screenWidth && y < screenHeight
 
 drawPoint :: V2 Int -> Color -> Ptr Word32 -> Int -> IO ()
 drawPoint v c ptr pitch = do
-  assertM (v, ptr, pitch) (checkBounds v)
+  assertM (v, ptr, pitch) (inScreenBounds v)
     pokeElemOff ptr (toOffset v pitch) (packColor c)
 
 toRad degrees = 2 * pi * ((fromIntegral degrees) / 360.0)
@@ -128,21 +134,27 @@ goof2 wordPtr pitch = do
                       | x <- [0..((fromIntegral screenWidth :: Int)-1)]
                       , y <- [0..((fromIntegral screenHeight :: Int)-1)]]
   drawPoint (V2 (screenWidth `div` 2) (screenHeight `div` 2)) (Color 255 255 255) wordPtr pitch
-  drawLine (V2 100 100) (V2 200 150) (Color 255 0 0) wordPtr pitch
-  drawLine (V2 100 100) (V2 150 200) (Color 255 0 0) wordPtr pitch
-  drawLine (V2 209 159) (V2 109 109) (Color 0 255 0) wordPtr pitch
-  drawLine (V2 159 209) (V2 109 109) (Color 0 255 0) wordPtr pitch
-  let dl (x, y) = drawLine (V2 x y) (V2 320 240) (Color 255 255 255) wordPtr pitch
-   in mapM_ dl ([(x, 200) | x <- [0, 10 .. 639]] ++
-                [(x, 280) | x <- [0, 10 .. 639]])
+  drawLine (Line (V2 100 100) (V2 200 150)) (Color 255 0 0) wordPtr pitch
+  drawLine (Line (V2 100 100) (V2 150 200)) (Color 255 0 0) wordPtr pitch
+  drawLine (Line (V2 209 159) (V2 109 109)) (Color 0 255 0) wordPtr pitch
+  drawLine (Line (V2 159 209) (V2 109 109)) (Color 0 255 0) wordPtr pitch
+  --let dl (x, y) = drawLine (Line (V2 x y) (V2 320 240)) (Color 255 255 255) wordPtr pitch
+   --in mapM_ dl ([(x, 200) | x <- [0, 10 .. 639]] ++
+                --[(x, 280) | x <- [0, 10 .. 639]])
   return ()
 
 goof3 theta wordPtr pitch = do
-  let dl' (V2 x y) = drawLine (V2 x y) (V2 320 240) (Color 128 255 255) wordPtr pitch
+  let dl' (V2 x y) = drawLine (Line (V2 x y) (V2 320 240)) (Color 128 255 255) wordPtr pitch
       step = 10
       startAng = toRad $ fromIntegral $ (floor ((fromIntegral theta) / 10.0)) `mod` step
    in mapM_ dl' [p + (V2 320 240) | p <- circlePoints 100 startAng (toRad step)]
   return ()
+
+data Line = Line (V2 Int) (V2 Int) deriving Show
+scale s (Line a b) = Line (s * a) (s * b)
+translate v (Line a b) = Line (v + a) (v + b)
+scaleLines s lines = map (scale s) lines
+translateLines v lines = map (translate v) lines
 
 -- World is made of unit cubes
 -- Cubes are identified by their least corner coords
@@ -156,10 +168,15 @@ worldMap_ = [
   "#      #",
   "#      #",
   "########" ]
-worldMap = [
+worldMap__ = [
   "###",
-  "# #",
+  "  #",
   "###"]
+worldMap = [
+  "####",
+  "#  #",
+  "#  #",
+  "## #"]
 
 world :: [[Bool]]
 world = map (\col -> map isWall col) (transpose worldMap)
@@ -168,17 +185,28 @@ world = map (\col -> map isWall col) (transpose worldMap)
     transpose ([]:_) = []
     transpose xs = (map head xs) : transpose (map tail xs)
 
-isHorWall x y = (isSolid x (y - 1)) /= (isSolid x y)
-isVerWall x y = (isSolid (x - 1) y) /= (isSolid x y)
-isSolid :: Int -> Int -> Bool
-isSolid x y = ((world !! x) !! y)
-
 allSameLength xs = length (nub xs) == 1
 worldIsSquare = allSameLength world
 blah = assert worldIsSquare ()
 worldSize = V2 (length (world !! 0)) (length world) 
-outsideWorld :: V2 Double -> Bool
-outsideWorld (V2 x y) = case worldSize of (V2 wx wy) -> x < -1 || x > (fromIntegral wx) + 1 || y < -1 || y > (fromIntegral wy) + 1
+outsideWorld :: Int -> Int -> Bool
+outsideWorld x y = x < 0 || y < 0 || x >= w || y >= h
+  where V2 w h = worldSize
+outsideWorldF :: V2 Double -> Bool
+outsideWorldF (V2 x y) = case worldSize of (V2 wx wy) -> x < 0 || x >= (fromIntegral wx) + 1 || y < -1 || y > (fromIntegral wy) + 1
+
+isHorWall x y = (isSolid x (y - 1)) /= (isSolid x y)
+isVerWall x y = (isSolid (x - 1) y) /= (isSolid x y)
+isSolid :: Int -> Int -> Bool
+isSolid x y
+  | outsideWorld x y = False
+  | otherwise = ((world !! x) !! y)
+
+horToLine x y = Line (V2 x y) (V2 (x + 1) y)
+verToLine x y = Line (V2 x y) (V2 x (y + 1))
+
+allWalls = [horToLine x y | x <- [0..w], y <- [0..h], isHorWall x y] ++ [verToLine x y | x <- [0..w], y <- [0..h], isVerWall x y]
+  where V2 w h = worldSize
 
 data WallPt = Ver Int Double | Hor Double Int deriving Show
 wallPtToV2 (Ver x y) = V2 (fromIntegral x) y
@@ -203,7 +231,7 @@ stepRay p0 p1 u s | TR.trace (show "stepRay " ++ (show p0) ++ " " ++ (show p1) +
 stepRay p0@(V2 x0 y0) p1@(V2 x1 y1) unitStep slope
   | (floor y0) /= (floor y1) && isHorWall (floor x0) (floor y1) && (abs slope) > 0 = Just $ Hor (x0 + (((fromIntegral (floor y1)) - y0) / slope)) (floor y1)
   | isVerWall (floor x1) (floor y1) = Just $ Ver (floor x1) y1
-  | outsideWorld p0 = Nothing
+  | outsideWorldF p0 = Nothing
   | otherwise = stepRay p1 (p1 + unitStep) unitStep slope
 
 {-
@@ -230,8 +258,12 @@ caseRayHor (V2 eyeX eyeY) (V2 dirX dirY) =
 --castRay :: V2
 -}
 
+drawMap t = withFramebuffer t (drawLines map)
+  where map = translateLines (V2 100 100) (scaleLines 50 allWalls)
+
 vroo = do
   putStrLn $ show $ V2 3.4 4.5
+  putStrLn $ show $ (V2 3.4 4.5) * 2
   putStrLn $ show $ Ver 1 2.3
   putStrLn $ show world
   putStrLn $ show $ (signorm (V2 1.0 0.5))
@@ -277,7 +309,8 @@ main = do
 
     loop theta = do
       --putStrLn $ "LOOP " ++ (show theta)
-      withFramebuffer targetTexture $ goof3 theta
+      --withFramebuffer targetTexture $ goof3 theta
+      drawMap targetTexture
       events <- map SDL.eventPayload <$> SDL.pollEvents
       let quit = SDL.QuitEvent `elem` events
 
