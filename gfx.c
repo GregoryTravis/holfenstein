b36d775496de35eabe8c9325a3ee9e94a23a4f2d
Gregory Travis <greg.m.travis@gmail.com>
2019-01-20 13:58:43 -0500

    zany colors

--- gfx.c
+++ gfx.c
@@ -10,5 +10,7 @@ void fastestFillVStrip(int32_t *start, int32_t count, int32_t step, int32_t colo
 #define CHECKSIZE 8
 
 int32_t sampler(int32_t x, int32_t y) {
-  return ((x / CHECKSIZE) % 2) == ((y / CHECKSIZE) % 2) ? 0xff0000ff : 0x00ff00ff;
+  //return ((x / CHECKSIZE) % 2) == ((y / CHECKSIZE) % 2) ? 0xff0000ff : 0x00ff00ff;
+  int check = ((x / CHECKSIZE) % 2) == ((y / CHECKSIZE) % 2) ? 0xff : 0x7f;
+  return ((((x<<1)+0x80) & check) << 24) | (((((63-x)<<1)+0x80) & check) << 16) | ((((y<<1)+0x80) & check) << 8) | 0xff;
 }
