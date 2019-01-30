9af4aa4aeb0ae86705c7fe70d745169a6edac2a5
Gregory Travis <greg.travis@listenfirstmedia.com>
2019-01-30 17:50:07 -0500

    wip

--- Diag.hs
+++ Diag.hs
@@ -21,6 +21,18 @@ drawDiag window diag = do
 
 v2p (V2 x y) = (x, y)
 
+-- scale, translate
+data Trans = Trans Double (V2 Double)
+
+transformDiag t (Diag lines) = Diag (map (transformLine t) lines)
+transformLine :: Trans -> V2 (V2 Double) -> V2 (V2 Double)
+transformLine trans (V2 a b) = V2 (transformPoint trans a) (transformPoint trans b)
+transformPoint :: V2 Double -> V2 Double
+transformPoint trans p = V2 (transformNum x) (transformNum y)
+transformNum x = (s * x) + 1
+
+boxToBoxTransform :: V2 (V2 Double) -> V2 (V2 Double) -> Trans
+
 bbox :: Diag -> V2 (V2 Double)
 bbox (Diag lines) = V2 (V2 minX minY) (V2 maxX maxY)
   where minX = minimum xs
