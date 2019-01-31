cc635545ccda01aacdc6b135165c5cb1046aa101
Gregory Travis <greg.travis@listenfirstmedia.com>
2019-01-31 17:53:53 -0500

    bbox transform

--- Diag.hs
+++ Diag.hs
@@ -14,6 +14,7 @@ drawDiag :: Window -> Diag -> IO ()
 drawDiag window diag = do
   msp diag
   msp $ bbox diag
+  msp $ boxToBoxTransform (V2 (V2 2.0 2.0) (V2 3.0 3.0)) (V2 (V2 1.0 1.0) (V2 3.0 3.0))
   return ()
 
 --pointsOf :: Diag -> [(V2 Double)]
@@ -22,16 +23,23 @@ drawDiag window diag = do
 v2p (V2 x y) = (x, y)
 
 -- scale, translate
-data Trans = Trans Double (V2 Double)
+data Trans = Trans Double (V2 Double) deriving Show
+
+composeTrans (Trans s0 t0) (Trans s1 t1) = Trans (s0 * s1) ((s1 *^ t0) + t1)
 
 transformDiag t (Diag lines) = Diag (map (transformLine t) lines)
 transformLine :: Trans -> V2 (V2 Double) -> V2 (V2 Double)
-transformLine trans (V2 a b) = V2 (transformPoint trans a) (transformPoint trans b)
-transformPoint :: V2 Double -> V2 Double
-transformPoint trans p = V2 (transformNum x) (transformNum y)
-transformNum x = (s * x) + 1
+transformLine t (V2 a b) = V2 (transformV t a) (transformV t b)
+transformV (Trans s t) v = (s *^ v) + t
 
 boxToBoxTransform :: V2 (V2 Double) -> V2 (V2 Double) -> Trans
+boxToBoxTransform (V2 ll0 ur0) (V2 ll1 ur1) =
+  composeTrans (Trans 1 (- ll0)) (composeTrans (Trans rescale (V2 0 0)) (Trans 1 ll1))
+  where rescale = min xscale yscale
+        (V2 w0 h0) = ur0 - ll0
+        (V2 w1 h1) = ur1 - ll1
+        xscale = w1 / w0
+        yscale = h1 / h0
 
 bbox :: Diag -> V2 (V2 Double)
 bbox (Diag lines) = V2 (V2 minX minY) (V2 maxX maxY)
--- Hoom.hs
+++ Hoom.hs
@@ -3,18 +3,21 @@ module Main (main) where
 import Control.Concurrent
 import Linear
 import System.Posix.Unistd
+import System.IO
+
 import Diag
 import Window
 
 (screenWidth, screenHeight) = (640, 480)
 
 main = do
+  hSetBuffering stdout NoBuffering
   window <- windowInit screenWidth screenHeight
 
   drawDiag window (Diag [(V2 (V2 3.0 3.0) (V2 5.0 4.0))])
 
   blit window
 
-  threadDelay 5000000
+  --threadDelay 5000000
 
   windowTerm window
