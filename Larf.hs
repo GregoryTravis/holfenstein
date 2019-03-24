066eaa8bf2e0823748ae3c575058e661456aa4d0
Gregory Travis <greg.m.travis@gmail.com>
2019-03-24 15:05:02 -0400

    shaderMain

--- Larf.hs
+++ Larf.hs
@@ -1,19 +1,10 @@
 module Main (main) where
 
-import Control.Concurrent
-import Control.Monad (unless)
-import Data.Char (ord)
-import Data.Maybe
-import qualified Data.Set as S
-import Data.Time.Clock.POSIX (getPOSIXTime)
-import System.Posix.Unistd
 import System.IO
 
 import Img
-import Math
 import Shader
 import Util
-import Window
 
 (screenWidth, screenHeight) = (640, 480)
 
@@ -22,25 +13,5 @@ shader t x y w h = Color (fade x w) (fade y h) 0
   where fade x m = (floor (256.0 * (((fromIntegral x) / (fromIntegral m)) + t))) `mod` 256
 
 main = do
-  --msp $ implicitCycletoExplicitCycle [(0, 1), (2, 0), (1, 2)]
   hSetBuffering stdout NoBuffering
-  window <- windowInit screenWidth screenHeight
-
-  let loop startTime = do
-        now <- getPOSIXTime
-        let startTime' = case startTime of Nothing -> Just now
-                                           Just x -> Just x
-        let elapsed = realToFrac $ now - (fromJust startTime')
-        --msp elapsed
-
-        runShader window (shader elapsed)
-        blit window
-        --msp "frame"
-
-        (cursorPos, newKeySet, quitEvent) <- getInput S.empty
-        let quit = quitEvent || S.member 27 newKeySet || S.member (ord 'q') newKeySet
-        unless quit $ loop startTime'
-        return ()
-   in loop Nothing
-
-  windowTerm window
+  shaderMain screenWidth screenHeight shader
--- Shader.hs
+++ Shader.hs
@@ -1,8 +1,17 @@
 module Shader
 ( runShader
+, shaderMain
 ) where
 
+import Control.Concurrent
+import Control.Monad (unless)
+import Data.Char (ord)
+import Data.Maybe
+import qualified Data.Set as S
+import Data.Time.Clock.POSIX (getPOSIXTime)
 import Linear
+import System.Posix.Unistd
+import System.IO
 
 import Grafix
 import Img
@@ -15,3 +24,25 @@ runShader window shader = withFramebuffer window renderPoints
         renderPoint ptr pitch (x, y) = drawPoint window (V2 x y) (packColor (shader x y w h)) ptr pitch
         renderPoints ptr pitch = mapM_ (renderPoint ptr pitch) allPoints
 
+shaderMain screenWidth screenHeight shader = do
+  --msp $ implicitCycletoExplicitCycle [(0, 1), (2, 0), (1, 2)]
+  window <- windowInit screenWidth screenHeight
+
+  let loop startTime = do
+        now <- getPOSIXTime
+        let startTime' = case startTime of Nothing -> Just now
+                                           Just x -> Just x
+        let elapsed = realToFrac $ now - (fromJust startTime')
+        --msp elapsed
+
+        runShader window (shader elapsed)
+        blit window
+        --msp "frame"
+
+        (cursorPos, newKeySet, quitEvent) <- getInput S.empty
+        let quit = quitEvent || S.member 27 newKeySet || S.member (ord 'q') newKeySet
+        unless quit $ loop startTime'
+        return ()
+   in loop Nothing
+
+  windowTerm window
